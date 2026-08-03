a3de06d8795827e3a3270930eccce3d6290067c7
5151561 <62234963+5151561@users.noreply.github.com>
2026-08-03 21:38:11 +0800

    1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         applicationId = "io.github.nodyssey"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
     }
 
     signingConfigs {
